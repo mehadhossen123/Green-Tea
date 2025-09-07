09f9d83c78868a27771a77203535be4b1bc42c06
Mehad Hossen <mehad004938@gmail.com>
2025-09-08 00:36:12 +0600

    add my file

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
 // All id is here 
 const categoryParent = document.getElementById("categoriesParen");
 const allComponentParent = document.getElementById("allComponentParent");
+const newsModalDetails = document.getElementById("news_modal_details");
+const eachModal = document.getElementById("modalId");
+const cartParent = document.getElementById("cartParent");
+
 
 
 
@@ -39,6 +43,7 @@ const showCategory=(category)=>{
             target.classList.add("bg-green-700");
         }
        showAllCategoryByName(e.target.id);
+     
 
         
     })
@@ -60,10 +65,10 @@ const fetchAllComponent=()=>{
 const showAllComponent=(data)=>{
     allComponentParent.innerHTML="";
     data.plants.forEach(element=>{
-        // console.log(element);
+       
         allComponentParent.innerHTML += `
 
-        <div class="shadow-lg  rounded-xl py-5 px-5" >
+        <div id="modalId" class="shadow-lg  rounded-xl py-5 px-5" >
           <img class="h-[300px]  rounded-t-xl w-full" src="${element.image}" alt="">
           <h1 class="font-bold mt-5">${element.name}</h1>
           <p class="">${element.description}</p>
@@ -91,21 +96,24 @@ const showAllCategoryByName=(id)=>{
 
 
 const displayByNameCategory=(card)=>{
+   
     allComponentParent.innerHTML="";
 
-    // console.log(card);
+    //  console.log(card.name);
     card.plants.forEach(element=>{
-        console.log(element);
+        
+        
+        //  console.log(element);
          allComponentParent.innerHTML += `
-          <div class="shadow-lg  rounded-xl py-5 px-5" >
+          <div  class="shadow-lg  rounded-xl py-5 px-5" >
           <img class="h-[300px]  rounded-t-xl w-full" src="${element.image}" alt="">
-          <h1 class="font-bold mt-5">${element.name}</h1>
+          <h1  onclick="showLoadDetails(${element.id})" class="font-bold mt-5 cursor-pointer">${element.name}</h1>
           <p class="">${element.description}</p>
           <div class="flex justify-between mt-4">
             <button class="border-2 border-green-400 px-4 rounded-xl  text-green-400">${element.category}</button>
             <p>$ <span>${element.price}</span></p>
           </div>
-          <button class="w-full mt-4 bg-green-700 py-2 rounded-xl">Add to Cart</button>
+          <button    class="w-full mt-4 bg-green-700 py-2 rounded-xl">Add to Cart</button>
         </div>
 
 
@@ -114,6 +122,61 @@ const displayByNameCategory=(card)=>{
     })
 
 }
+
+    
+   allComponentParent.addEventListener('click',(e)=>{
+    if(e.target.innerText==="Add to Cart"){
+        const title=e.target.parentNode.children[3].children[0].innerText;
+        const treePrice=e.target.parentNode.children[3].children[1].innerText;
+        const totalPrice = document.getElementById("TotalPrice");
+       cartParent.innerHTML += `
+       <div class="flex justify-between items-center mt-4">
+   <div> <h1>${title}</h1>
+  <p>${treePrice}</p>
+  </div>
+  <div class=""><button class="btn btn-outline btn-error text-red-700">Delete</button></div>
+
+</div>
+
+       `;
+    }
+   
+   })
+
+const showLoadDetails=(id)=>{
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayModals(data.plants));
+    
+
+}
+const displayModals=(element)=>{
+    eachModal.innerHTML="";
+    console.log(element);
+    eachModal.innerHTML += `
+    <div  class="shadow-xl  rounded-xl py-5 px-5" >
+     <h1  onclick="showLoadDetails(${element.id})" class="font-bold mb-5 cursor-pointer text-2xl">${element.name}</h1>
+    
+          <img class="h-[200px]  rounded-t-xl w-full" src="${element.image}" alt="">
+          <h1 class=""> <span class="font-bold">Category:</span> ${element.category}</h1>
+            <p> <span class="font-bold">Price:</span>$ <span>${element.price}</span></p>
+          <p class=""><span class="font-bold">Description: </span> ${element.description}</p>
+         
+         
+          <div class="flex justify-between mt-4">
+           
+          
+          </div>
+         
+        </div>
+
+    `;
+     newsModalDetails.showModal();
+}
+
+
 fetchAllComponent();
 
 fetchCategory();
