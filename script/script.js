354a273c9cea2dbad297c4e3f56e0d264fe685b7
Mehad Hossen <mehad004938@gmail.com>
2025-09-07 14:54:49 +0600

    please add my category part

--- script/script.js
+++ script/script.js
@@ -1,1 +1,57 @@
+// All id is here 
+const categoryParent = document.getElementById("categoriesParen");
+const allCategoryParent = document.getElementById("allCategoryParent");
+
+
+
+
+// fetch category 
+const fetchCategory=()=>{
+    const url= "https://openapi.programming-hero.com/api/categories"
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => showCategory(data));
+    
+   
+    
+}
+const showCategory=(category)=>{
+    categoryParent.innerHTML="";
+    // console.log(category.categories);
+    category.categories.forEach((element) => {
+    //  console.log(element);
+    categoryParent.innerHTML += `
+    <li id="${element.id}" class="list-none text-center hover:bg-green-700 cursor-pointer font-bold hover:text-white  hover:rounded-lg ">${element.category_name}
+    </li>
+    
+    `;
+
+    });
+    categoryParent.addEventListener('click',(e)=>{
+        const allLi=document.querySelectorAll('li');
+       allLi.forEach(li=>{
+        li.classList.remove("bg-green-700");
+       })
+        
+        // console.log(e.target);
+        const target = e.target;
+        if(target.localName==="li"){
+            target.classList.add("bg-green-700");
+        }
+
+        
+    })
+    
+
+}
+
+fetchCategory();
+
+
+
+
+
+
+
+
 
