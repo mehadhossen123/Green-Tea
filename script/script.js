e3a381bb746fea64fdc547f0f7ecdf9e7ecaac5a
Mehad Hossen <mehad004938@gmail.com>
2025-09-07 16:29:27 +0600

    add my default part

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 // All id is here 
 const categoryParent = document.getElementById("categoriesParen");
-const allCategoryParent = document.getElementById("allCategoryParent");
+const allComponentParent = document.getElementById("allComponentParent");
 
 
 
@@ -44,6 +44,40 @@ const showCategory=(category)=>{
     
 
 }
+// fetch all default component
+const fetchAllComponent=()=>{
+    const url = "https://openapi.programming-hero.com/api/plants"
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>showAllComponent(data)
+    )
+   .catch(err=>{
+    // console.log(err);
+   })
+}
+
+const showAllComponent=(data)=>{
+    allComponentParent.innerHTML="";
+    data.plants.forEach(element=>{
+        console.log(element);
+        allComponentParent.innerHTML += `
+
+        <div class="shadow-lg  rounded-xl py-5 px-5" >
+          <img class="h-[300px]  rounded-t-xl w-full" src="${element.image}" alt="">
+          <h1 class="font-bold mt-5">${element.name}</h1>
+          <p class="">${element.description}</p>
+          <div class="flex justify-between mt-4">
+            <button class="border-2 border-green-400 px-4 rounded-xl  text-green-400">${element.category}</button>
+            <p>$ <span>${element.price}</span></p>
+          </div>
+          <button class="w-full mt-4 bg-green-700 py-2 rounded-xl">Add to Cart</button>
+        </div>
+        
+        `;
+        
+    })
+}
+fetchAllComponent();
 
 fetchCategory();
 
